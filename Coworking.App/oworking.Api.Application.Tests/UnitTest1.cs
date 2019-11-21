563e5bc3e6323023df58dac419de94cd6b3aa6b4
kherrera <kenher822@gmail.com>
2019-11-20 20:29:00 -0600

    Agregando capas del proyecto

---
+++ Coworking.App/oworking.Api.Application.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace oworking.Api.Application.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
